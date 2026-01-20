07d56fa938c41ba5d7b2f2b9e8ff2ff8da74e66c
bryanhoffman <bryanhoffman1@gmail.com>
2026-01-19 21:04:09 -0600

    Simulated the heat equation using finite differences. Starting at f_0 = 0 and f_1 = 100, with all intermediate values set to 100, the function iteratively approaches a solution y=100x (see "heat equation with fixed boundary solved by finite differences.png" for a visualization)

---
+++ 1.7 Finite Differences.py
@@ -0,0 +1,34 @@
+from time import sleep
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+N=100
+meshPoints=2*N+1
+deltaT = 1
+x_vals = [0]*meshPoints
+for i in range(0,meshPoints):
+    x_vals[i]= i/(meshPoints-1);
+f = [0]+[100]*(meshPoints-1)
+
+for i in range(500000):
+    change = [0]*meshPoints
+    change[1]=(-f[0]+2*f[1])/(4*N**2)
+    for x in range(2,meshPoints-2):
+        change[x] = (-f[x-1]+2*f[x]-f[x+1])/(4*N**2)
+    change[meshPoints-2] = (-f[meshPoints-2]+2*f[meshPoints-1])/(4*N**2)
+
+    for x in range(1,meshPoints-2):
+        f[x]=f[x]-10000*change[x]
+
+    if(i%1000==0):
+        ax.plot(x_vals, f)
+
+
+
+
+plt.show()
+
+
+
